9e166172a8480f51fb50d574ec8f238d8b8489f6
JRTest <jonathan@norraspanien.se>
2021-05-11 12:53:54 +0200

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const panels = document.querySelectorAll(".panel");
 
 //console.log(panels);
-//console.log(panels[0]);
+console.log(panels[0]);
 
 panels.forEach((panel) => {
-  // add an eventlistener to listen to the click
   panel.addEventListener("click", () => {
     removeActiveClass();
 
@@ -14,7 +13,9 @@ panels.forEach((panel) => {
 
 function removeActiveClass() {
   panels.forEach((panel) => {
-    // remove all active panel classes
     panel.classList.remove("active");
   });
 }
+
+
+console.log(panels);
